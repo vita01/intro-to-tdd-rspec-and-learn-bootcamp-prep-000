fb1bc12c564136463b704a20b128ce7c082b0a14
Victoria <vitalina27@gmail.com>
2017-12-02 12:18:59 +0000

    automatically backed up by learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,7 +1,5 @@
 require_relative './current_age_for_birth_year.rb'
-def current_age_for_birth_year(birth_year)
-  Time.now.year - birth_year
-end
+
 puts "What year were you born?"
 birth_year = gets.to_i
 
